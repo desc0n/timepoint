19f727de0ebdea24db8e9d664fa76f5390e14f28
desc0n <desconbk@yandex.ru>
2017-05-06 18:00:14 +1000

    Редактирую стили

--- application/views/template.php
+++ application/views/template.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,102 +10,76 @@
     <link rel="stylesheet" href="/public/css/styles.css" >
 </head>
 <body>
-<div class="container">
-    <nav class="navbar navbar-toggleable-md navbar-inverse">
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
-                data-target="#navbar" aria-controls="navbar" aria-expanded="false"
-                aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <a class="navbar-brand" href="/"><img src="/public/images/logo.svg" alt="VladPoint"></a>
+<div class="wrapper">
+    <div class="container">
+        <nav class="navbar navbar-toggleable-md navbar-inverse">
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+                    data-target="#navbar" aria-controls="navbar" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="/"><img src="/public/images/logo.svg" alt="VladPoint"></a>
 
-        <div class="collapse navbar-collapse" id="navbar">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Гостиница</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Номера и цены</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Новости</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Контакты</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-link__booking" href="#">Бронирование</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" id="dropdown1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">RU</a>
-                    <div class="dropdown-menu" aria-labelledby="dropdown1">
-                        <a class="dropdown-item" href="#">RU</a>
-                        <a class="dropdown-item" href="#">EN</a>
-                    </div>
-
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" id="dropdown2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">RUB</a>
-                    <div class="dropdown-menu" aria-labelledby="dropdown2">
-                        <a class="dropdown-item" href="#">RUB</a>
-                        <a class="dropdown-item" href="#">USD</a>
-                    </div>
-
-                </li>
-                <li class="nav-item nav-item__contacts hidden-lg-down">
-                    <img src="/public/images/point.png" alt="Адрес">
-                    <div class="nav-link" >
-                        <div class="nav-link__address">г. Владивосток, ул. Посьетская, 14
+            <div class="collapse navbar-collapse" id="navbar">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Гостиница</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Номера и цены</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Новости</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Контакты</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link nav-link__booking" href="#">Бронирование</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" id="dropdown1" data-toggle="dropdown" aria-haspopup="true"
+                           aria-expanded="true">RU</a>
+                        <div class="dropdown-menu" aria-labelledby="dropdown1">
+                            <a class="dropdown-item" href="#">RU</a>
+                            <a class="dropdown-item" href="#">EN</a>
                         </div>
-                        <div class="nav-link__phone">8 800 235 35 72</div>
-                    </div>
-                </li>
-            </ul>
-        </div>
-    </nav>
-</div>
-<!-- /.navbar -->
-<div id="carousel" class="carousel slide" data-ride="carousel">
-    <ol class="carousel-indicators">
-        <li data-target="#carousel" data-slide-to="0" class="active"><img class="d-block img-fluid" src="/public/images/c1.png"></li>
-        <li data-target="#carousel" data-slide-to="1" class=""><img class="d-block img-fluid" src="/public/images/c1.png"></li>
-        <li data-target="#carousel" data-slide-to="2" class=""><img class="d-block img-fluid" src="/public/images/c1.png"></li>
-        <li data-target="#carousel" data-slide-to="3" class=""><img class="d-block img-fluid" src="/public/images/c1.png"></li>
-    </ol>
-    <div class="carousel-inner" role="listbox">
-        <div class="carousel-item  active">
-            <img class="d-block img-fluid" src="/public/images/c1.png">
-            <div class="carousel-caption d-none d-md-block">
-                <h1>МИНИ ОТЕЛЬ В ДЕЛОВОМ ЦЕНТРЕ ВЛАДИВОСТОКА<br/> С ВИДОМ НА БУХТУ «ЗОЛОТОЙ РОГ»</h1>
-                <img src="/public/images/logo.svg" alt="VladPoint">
-            </div>
-        </div>
-        <div class="carousel-item">
-            <img class="d-block img-fluid" src="/public/images/c1.png">
-            <div class="carousel-caption d-none d-md-block">
-                <h1>МИНИ ОТЕЛЬ В ДЕЛОВОМ ЦЕНТРЕ ВЛАДИВОСТОКА<br/> С ВИДОМ НА БУХТУ «ЗОЛОТОЙ РОГ»</h1>
-                <img src="/public/images/logo.svg" alt="VladPoint">
-            </div>
-        </div>
-        <div class="carousel-item">
-            <img class="d-block img-fluid" src="/public/images/c1.png">
-            <div class="carousel-caption d-none d-md-block">
-                <h1>МИНИ ОТЕЛЬ В ДЕЛОВОМ ЦЕНТРЕ ВЛАДИВОСТОКА<br/> С ВИДОМ НА БУХТУ «ЗОЛОТОЙ РОГ»</h1>
-                <img src="/public/images/logo.svg" alt="VladPoint">
 
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" id="dropdown2" data-toggle="dropdown" aria-haspopup="true"
+                           aria-expanded="true">RUB</a>
+                        <div class="dropdown-menu" aria-labelledby="dropdown2">
+                            <a class="dropdown-item" href="#">RUB</a>
+                            <a class="dropdown-item" href="#">USD</a>
+                        </div>
+
+                    </li>
+                    <li class="nav-item nav-item__contacts hidden-lg-down">
+                        <img src="/public/images/point.png" alt="Адрес">
+                        <div class="nav-link">
+                            <div class="nav-link__address">г. Владивосток, ул. Посьетская, 14
+                            </div>
+                            <div class="nav-link__phone">8 800 235 35 72</div>
+                        </div>
+                    </li>
+                </ul>
             </div>
-        </div>
-        <div class="carousel-item">
-            <img class="d-block img-fluid" src="/public/images/c1.png">
-            <div class="carousel-caption d-none d-md-block">
-                <h1>МИНИ ОТЕЛЬ В ДЕЛОВОМ ЦЕНТРЕ ВЛАДИВОСТОКА<br/> С ВИДОМ НА БУХТУ «ЗОЛОТОЙ РОГ»</h1>
-                <img src="/public/images/logo.svg" alt="VladPoint">
+        </nav>
+    </div>
+    <!-- /.navbar -->
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="header">
+                    <h1>МИНИ ОТЕЛЬ В ДЕЛОВОМ ЦЕНТРЕ ВЛАДИВОСТОКА С ВИДОМ НА ГОРОД И БУХТУ «ЗОЛОТОЙ РОГ»</h1>
+                    <img src="/public/images/logo.svg" alt="VladPoint">
+                </div>
             </div>
         </div>
     </div>
-
 </div>
-<!-- /.carousel -->
+<!-- /.wrapper -->
 <div class="container">
     <div class="booking">
         <div class="row">
@@ -118,28 +93,30 @@
 </div>
 <div class="container">
     <div class="rooms">
-        <div class="rooms__kind rooms__kind_a">
-            <div class="rooms__kind-caption">
-                <div class="rooms__kind-caption-price">от 2000 руб.</div>
-                <button type="button" class="btn btn-primary rooms__kind-caption-action">Забронировать</button>
+        <div class="rooms__wrapper">
+            <div class="rooms__kind rooms__kind_a">
+                <div class="rooms__kind-caption">
+                    <div class="rooms__kind-caption-price">от 2000 руб.</div>
+                    <button type="button" class="btn btn-primary rooms__kind-caption-action">Забронировать</button>
+                </div>
             </div>
-        </div>
-        <div class="rooms__kind rooms__kind_b">
-            <div class="rooms__kind-caption">
-                <div class="rooms__kind-caption-price">от 3000 руб.</div>
-                <button type="button" class="btn btn-primary rooms__kind-caption-action">Забронировать</button>
+            <div class="rooms__kind rooms__kind_b">
+                <div class="rooms__kind-caption">
+                    <div class="rooms__kind-caption-price">от 3000 руб.</div>
+                    <button type="button" class="btn btn-primary rooms__kind-caption-action">Забронировать</button>
+                </div>
             </div>
-        </div>
-        <div class="rooms__kind rooms__kind_c">
-            <div class="rooms__kind-caption">
-                <div class="rooms__kind-caption-price">от 4000 руб.</div>
-                <button type="button" class="btn btn-primary rooms__kind-caption-action">Забронировать</button>
+            <div class="rooms__kind rooms__kind_c">
+                <div class="rooms__kind-caption">
+                    <div class="rooms__kind-caption-price">от 4000 руб.</div>
+                    <button type="button" class="btn btn-primary rooms__kind-caption-action">Забронировать</button>
+                </div>
             </div>
-        </div>
-        <div class="rooms__kind rooms__kind_d">
-            <div class="rooms__kind-caption">
-                <div class="rooms__kind-caption-price">от 5000 руб.</div>
-                <button type="button" class="btn btn-primary rooms__kind-caption-action">Забронировать</button>
+            <div class="rooms__kind rooms__kind_d">
+                <div class="rooms__kind-caption">
+                    <div class="rooms__kind-caption-price">от 5000 руб.</div>
+                    <button type="button" class="btn btn-primary rooms__kind-caption-action">Забронировать</button>
+                </div>
             </div>
         </div>
     </div>
@@ -153,7 +130,7 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-4">
+            <div class="col-6">
                 <div class="contacts-block__caption">
                     Телефон бронирования
                 </div>
@@ -161,26 +138,11 @@
                     8 800 235 35 72
                 </div>
             </div>
-            <div class="col-4">
+            <div class="col-6">
                 <div class="contacts-block__caption">
-                    Стойка администратора
+                    Адрес
                 </div>
                 <div class="contacts-block__content">
-                    8 423 73 62 44
-                </div>
-            </div>
-            <div class="col-4">
-                <div class="contacts-block__caption">
-                    Директор
-                </div>
-                <div class="contacts-block__content">
-                    8 423 235 82 31
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-12">
-                <div class="contacts-block__address">
                     <img src="/public/images/point.png" alt="Адрес">
                     г. Владивосток, ул. Посьетская, 14
                 </div>

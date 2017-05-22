f98c383f6d67c5baa5d854232786b4b884b950da
desc0n <desconbk@yandex.ru>
2017-05-23 00:36:37 +1000

    Назначаю версию стилям

--- application/views/template.php
+++ application/views/template.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="/public/css/styles.css?v=3" >
+    <link rel="stylesheet" href="/public/css/styles.css?v=4" >
 </head>
 <?php
 /** @var Model_Content $contentModel */

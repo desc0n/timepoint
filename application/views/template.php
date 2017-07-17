3d50f0bdfcf2c01fba5e0fff3c63315ab49824d2
desc0n <desconbk@yandex.ru>
2017-07-17 23:38:37 +1000

    Добавляю fav

--- application/views/template.php
+++ application/views/template.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
+    <link rel="icon" href="/public/images/fav.png" sizes="38x38" type="image/png">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <link rel="stylesheet" href="/public/css/styles.css?v=7" >

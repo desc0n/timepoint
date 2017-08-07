e2f509ed0c850c158acdc5d05b23d2736123aea9
desc0n <desconbk@yandex.ru>
2017-08-07 20:44:35 +1000

    Меняю текст в окне при бронировании

--- application/views/reservation_modal.php
+++ application/views/reservation_modal.php
@@ -1,4 +1,7 @@
-<div class="alert alert-danger text-center">При бронировании без оплаты, Ваша бронь будет действительна в течении суток</div>
+<div class="alert alert-danger text-center">
+<!--    При бронировании без оплаты, Ваша бронь будет действительна в течении 24 часов.-->
+    <strong>Бронирование без оплаты прекращается за 5 суток до предполагаемой даты заезда.</strong>
+</div>
 <div class="row">
     <div class="form-group col-lg-6 col-md-6 col-sm-12 col-xs-12">
         <button class="btn" onclick="notPayedReserveRoom();">Забронировать без оплаты</button>
